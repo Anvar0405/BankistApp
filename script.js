124cfa0b93469a66315342d833107fdadaba5964
Anvar0405 <anvarotamurodov0405@gmail.com>
2024-10-12 20:07:23 +0500

    first commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
 // DIFFERENT DATA! Contains movement dates, currency and locale
 
 const account1 = {
-  owner: 'Azizbek😎 Jurayev',
+  owner: 'Anvar Otamurodov',
   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -32,7 +32,7 @@ const account1 = {
 };
 
 const account2 = {
-  owner: 'Anvar😘 Otamurodov',
+  owner: 'Azizbek Jurayev',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
@@ -51,45 +51,45 @@ const account2 = {
   locale: 'en-US',
 };
 
-// const account3 = {
-//   owner: 'Azizbek😎 Jurayev',
-//   movements: [500, 300, -1500, -790, -3210, 1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 2004,
-//   movementsDates: [
-//     '2019-11-01T13:15:33.035Z',
-//     '2019-11-30T09:48:16.867Z',
-//     '2019-12-25T06:04:23.907Z',
-//     '2020-01-25T14:18:46.235Z',
-//     '2020-02-05T16:33:06.386Z',
-//     '2020-04-10T14:43:26.374Z',
-//     '2020-06-25T18:49:59.371Z',
-//     '2020-07-26T12:01:20.894Z',
-//   ],
-//   currency: 'USD',
-//   locale: 'ar-sy',
-// };
+const account3 = {
+  owner: 'Diyorbek Bazarov',
+  movements: [500, 300, -1500, -790, -3210, 1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2004,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'ar-sy',
+};
 
-// const account4 = {
-//   owner: 'Lazizbek😘 Xolmamatov',
-//   movements: [500, 300, -1500, -790, -3210, 1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 4404,
-//   movementsDates: [
-//     '2019-11-01T13:15:33.035Z',
-//     '2019-11-30T09:48:16.867Z',
-//     '2019-12-25T06:04:23.907Z',
-//     '2020-01-25T14:18:46.235Z',
-//     '2020-02-05T16:33:06.386Z',
-//     '2020-04-10T14:43:26.374Z',
-//     '2020-06-25T18:49:59.371Z',
-//     '2020-07-26T12:01:20.894Z',
-//   ],
-//   currency: 'USD',
-//   locale: 'en-En',
-// };
+const account4 = {
+  owner: 'Abdulloh Akbarov',
+  movements: [500, 300, -1500, -790, -3210, 1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 4404,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-En',
+};
 
-const accounts = [account1, account2];
+const accounts = [account1, account2, account3, account4];
 
 /////////////////////////////////////////////////
 // Elements
